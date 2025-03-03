a44bafb98dfb859b64386327cb94d7516948edd5
CauePukall <cauegpukall@gmail.com>
2025-03-03 18:38:41 +0000

    Desafio movimentaçao das peças de xadrez!

---
+++ xadrez2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    
+    int c1, c;
+
+    printf("Movimento do Cavalo:\n");
+
+    for (c = 0; c < 2; c++) 
+    {
+        printf("Baixo\n");
+    }
+    while (c1 < 1)
+    {
+        printf("Esquerda\n");
+        c1++;
+    }
+
+    printf("\n");
+
+    return 0;
+}
